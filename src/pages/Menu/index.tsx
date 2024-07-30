25568ad6655f8713834c8e4c18ad6e58f66f2b98
Gabriel-mss <gabrielnumero47@gmail.com>
2024-07-29 22:11:34 -0300

    Atualizações e criação do "Buscador"

--- src/pages/Menu/index.tsx
+++ src/pages/Menu/index.tsx
@@ -1,12 +1,27 @@
-import styles from './Menu.module.scss'
-import { ReactComponent as Logo } from 'assets/logo.svg'
+import { useState } from "react";
+import Buscador from "./Buscador";
+import styles from "./Menu.module.scss";
+import { ReactComponent as Logo } from "assets/logo.svg";
 
-export default function Menu () {
-    return(
-        <main>
-            <nav className={styles.menu}>
-                <Logo />
-            </nav>
-        </main>
-    )
+export default function Menu() {
+  const [busca, setBusca] = useState("");
+  return (
+    <main>
+      <nav className={styles.menu}>
+        <Logo />
+      </nav>
+      <header className={styles.header}>
+        <div className={styles.header__text}>
+            A casa do código e da massa
+        </div>
+      </header>
+      <section className={styles.cardapio}>
+        <h3 className={styles.cardapio__titulo}>Cardápio</h3>
+        <Buscador 
+            busca={busca} 
+            setBusca={setBusca} 
+        />
+      </section>
+    </main>
+  );
 }
