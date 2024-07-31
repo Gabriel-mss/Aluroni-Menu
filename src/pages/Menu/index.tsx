4726450037819ab0315d0ba2eac2de1fa3d5911d
Gabriel-mss <gabrielnumero47@gmail.com>
2024-07-30 22:35:59 -0300

    Criação-ordenador

--- src/pages/Menu/index.tsx
+++ src/pages/Menu/index.tsx
@@ -2,25 +2,28 @@ import { useState } from "react";
 import Buscador from "./Buscador";
 import styles from "./Menu.module.scss";
 import { ReactComponent as Logo } from "assets/logo.svg";
+import Filtros from "./Filtros";
+import Ordenador from "./Ordenador";
 
 export default function Menu() {
   const [busca, setBusca] = useState("");
+  const [filtro, setFiltro] = useState<number | null>(null)
+  const [ordenador, setOrdenador] = useState("")
   return (
     <main>
       <nav className={styles.menu}>
         <Logo />
       </nav>
       <header className={styles.header}>
-        <div className={styles.header__text}>
-            A casa do código e da massa
-        </div>
+        <div className={styles.header__text}>A casa do código e da massa</div>
       </header>
       <section className={styles.cardapio}>
         <h3 className={styles.cardapio__titulo}>Cardápio</h3>
-        <Buscador 
-            busca={busca} 
-            setBusca={setBusca} 
-        />
+        <Buscador busca={busca} setBusca={setBusca} />
+        <div className={styles.menu__filtros}>
+          <Filtros filtro={filtro} setFiltro={setFiltro}/>
+          <Ordenador ordenador={ordenador} setOrdenador={setOrdenador} />
+        </div>
       </section>
     </main>
   );
